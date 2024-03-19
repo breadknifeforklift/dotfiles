cfc85119c32f5cd2e11fbd7861c37c5c054c8aa1
breadknifeforklift <breadknifeforklift@proton.me>
2024-03-19 17:14:49 -0400

    flake template

--- flake.nix
+++ flake.nix
@@ -1,47 +1,18 @@
 {
-  description = "Nixos config flake";
-  # what is consumed (previously provided by channels and fetchTarball)
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    impermanence.url = "github:Nix-community/impermanence";
-    
-    # Home manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  description = "A flake";
 
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  outputs = { self }: {
 
-  # what will be produced (i.e. the build)
-  outputs = {
-    self,
-    nixpkgs,
-    impermanence,
-    home-manager,
-    ...
-  }@inputs: {
-    templates.default = {
-      description = "My default template";
-      path = ".";
-    };
+    templates = {
 
-    nixosConfigurations = let device = "sda"; in {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs device; }; # forward inputs to modules
-        modules = [
-          inputs.disko.nixosModules.default
-          (import ./disko.nix)
-
-          ./configuration.nix
-          inputs.home-manager.nixosModules.default
-          inputs.impermanence.nixosModules.impermanence
-        ];
+     mytemplate = {
+        path = ./mytemplate;
+        description = "Impermanent flake";
       };
+
     };
+
+    defaultTemplate = self.templates.mytemplate;
+
   };
 }

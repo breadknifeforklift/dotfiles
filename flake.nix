9df58441cc5a1fbe4cf4d06a762e9c52e606552c
breadknifeforklift <breadknifeforklift@proton.me>
2024-03-19 15:57:09 -0400

    change scope of device variable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Nixos config flake";
-  device = "sda";
   # what is consumed (previously provided by channels and fetchTarball)
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
@@ -26,22 +25,27 @@
     home-manager,
     ...
   }@inputs: {
-    templates.default = {
-      description = "My default template";
-      path = ".";
-    };
+    let
+      device = "sda";
+    in
+    {
+      templates.default = {
+        description = "My default template";
+        path = ".";
+      };
 
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs device; }; # forward inputs to modules
-        modules = [
-          inputs.disko.nixosModules.default
-          (import ./disko.nix)
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs device; }; # forward inputs to modules
+          modules = [
+            inputs.disko.nixosModules.default
+            (import ./disko.nix)
 
-          ./configuration.nix
-          inputs.home-manager.nixosModules.default
-          inputs.impermanence.nixosModules.impermanence
-        ];
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+            inputs.impermanence.nixosModules.impermanence
+          ];
+        };
       };
     };
   };

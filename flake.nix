fb54d76b4b77e7623d701e8aab9353fd5e1f2496
breadknifeforklift <breadknifeforklift@proton.me>
2024-03-26 16:21:44 -0400

    change to unstable

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Nixos config flake";
   # what is consumed (previously provided by channels and fetchTarball)
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     impermanence.url = "github:Nix-community/impermanence";
     
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
